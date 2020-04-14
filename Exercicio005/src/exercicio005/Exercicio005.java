9bc63eba2409f502a329a8af1ed25ec2512a70da
Vitor Martins Cruz <vitormartinscruz@yahoo.com>
2020-04-14 20:35:17 -0300

    Exercise update

--- Exercicio005/src/exercicio005/Exercicio005.java
+++ Exercicio005/src/exercicio005/Exercicio005.java
@@ -13,7 +13,9 @@ public class Exercicio005 {
         l[4] = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
         l[5] = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
         
-        l[0].status();
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar(l[0], l[5]);
         
         
     }
