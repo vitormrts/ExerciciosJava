c1c75e2308695904c671f8b682881b2de588fdfd
Vitor Martins Cruz <vitormartinscruz@yahoo.com>
2020-05-04 21:03:26 -0300

    Adding exercises

---
+++ uri-onlinejudge/1049.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+ 
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        String b = input.nextLine();
+        String c = input.nextLine();
+        
+        if ("vertebrado".equals(a)) {
+            if ("ave".equals(b)) {
+                if ("carnivoro".equals(c)) 
+                    System.out.println("aguia");
+                else
+                    System.out.println("pomba");
+            }
+            else {
+                if ("onivoro".equals(c))
+                    System.out.println("homem");
+                else 
+                    System.out.println("vaca");
+            }
+                
+        }
+        
+        else {
+            if ("inseto".equals(b)) {
+                if ("hematofago".equals(c))
+                    System.out.println("pulga");
+                else 
+                    System.out.println("lagarta");             
+            }
+            else {
+                if ("hematofago".equals(c))
+                    System.out.println("sanguessuga");
+                else 
+                    System.out.println("minhoca");
+            }
+        }
+ 
+    }
+ 
+}
