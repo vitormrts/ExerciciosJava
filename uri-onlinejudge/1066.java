7fc16d5cd6c70c4e3e2a2b1f87b90eecd05820d5
Vitor Martins Cruz <vitormartinscruz@yahoo.com>
2020-05-07 21:59:59 -0300

    Adding exercises

---
+++ uri-onlinejudge/1066.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+ 
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+        
+        int pares = 0;
+        int impares = 0;
+        int positivos = 0;
+        int negativos = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            int num = input.nextInt();
+            
+            if (num % 2 == 0) pares ++;
+            else impares ++;
+            
+            if (num>0) positivos ++;
+            else if (num<0) negativos ++;
+            
+        }
+        
+        System.out.println(pares + " valor(es) par(es)");   
+        System.out.println(impares + " valor(es) impar(es)");   
+        System.out.println(positivos + " valor(es) positivo(s)");   
+        System.out.println(negativos + " valor(es) negativo(s)");   
+ 
+    }
+ 
+}
